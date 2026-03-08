c49e2d71dd3094e691ac644112c160d4eb3b6847
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-03-07 22:22:13 -0300

    concluido exercicio 4

--- Python/exercicios/desafio_04.py
+++ Python/exercicios/desafio_04.py
@@ -2,3 +2,10 @@
 # pelo teclado e mostre na tela o seu tipo primitivo
 # e todas as informações possiveis sobre ele
 
+variavel = input('Digite algo: ')
+print(type(variavel), variavel, variavel.isalnum())
+print(type(variavel), variavel, variavel.isalpha())
+print(type(variavel), variavel, variavel.isdigit())
+print(type(variavel), variavel, variavel.isupper())
+print(type(variavel), variavel, variavel.islower())
+print(type(variavel), variavel, variavel.isnumeric())
