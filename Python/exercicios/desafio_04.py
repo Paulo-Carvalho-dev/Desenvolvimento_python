1780e8c8f6af5f1f0385ee58308b1e8958ae1034
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-03-07 22:43:07 -0300

    concluido exercicio 4

--- Python/exercicios/desafio_04.py
+++ Python/exercicios/desafio_04.py
@@ -3,9 +3,11 @@
 # e todas as informações possiveis sobre ele
 
 variavel = input('Digite algo: ')
-print(type(variavel), variavel, variavel.isalnum())
-print(type(variavel), variavel, variavel.isalpha())
-print(type(variavel), variavel, variavel.isdigit())
-print(type(variavel), variavel, variavel.isupper())
-print(type(variavel), variavel, variavel.islower())
-print(type(variavel), variavel, variavel.isnumeric())
+print(f'O tipo primitivo desse valor é {type(variavel)}')
+print(f'Só tem espaços {variavel.isspace()}')
+print(f'É um número? {variavel.isnumeric()}')
+print(f'É alfabetico? {variavel.isalpha()}')
+print(f'É alfanumerico? {variavel.isalnum()}')
+print(f'Está em maiuscula? {variavel.isupper()}')
+print(f'Está em minuscula? {variavel.islower()}')
+print(f'Está capitalizado? {variavel.istitle()}')
