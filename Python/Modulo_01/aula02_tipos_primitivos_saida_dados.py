3f4e1bfb15319609f660660a0e4fa64ae8465c5e
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-03-07 21:22:30 -0300

    entendendo mais sobre formatação de strings e fazendo coerção de tipos de dados

--- Python/Modulo_01/aula02_tipos_primitivos_saida_dados.py
+++ Python/Modulo_01/aula02_tipos_primitivos_saida_dados.py
@@ -1,2 +1,10 @@
 # Tipos primitivos saida de dados
 
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+soma = n1 + n2
+
+# Format sintax mais antiga
+print('A soma entre {} e {} vale {}'.format(n1, n2, soma))
+# f-string mais nova
+print(f'A soma entre {n1} e {n2} vale {soma}')
