a82c859ed87a03ab368e21ef74c1c1d1afa72d83
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-03-09 16:06:57 -0300

    (refector) exericios horas

--- curso_python3_basico_avancado/exercicios/ex06.py
+++ curso_python3_basico_avancado/exercicios/ex06.py
@@ -6,13 +6,27 @@ Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23
 
 hora_dia = input('Digite a hora do dia: ')
 
-hora_dia_int = int(hora_dia)
+# hora_dia_int = int(hora_dia)
 
-if hora_dia_int >= 0 and hora_dia_int <= 11:
-    print('BOM DIA')
-elif hora_dia_int >= 12 and hora_dia_int <= 17:
-    print('BOA TADE')
-elif hora_dia_int >= 18 and hora_dia_int <= 23:
-    print('BOA NOITE')
-else:
-    print('Você não digitou uma hora valida.')
+# if hora_dia_int >= 0 and hora_dia_int <= 11:
+#     print('BOM DIA')
+# elif hora_dia_int >= 12 and hora_dia_int <= 17:
+#     print('BOA TADE')
+# elif hora_dia_int >= 18 and hora_dia_int <= 23:
+#     print('BOA NOITE')
+# else:
+#     print('Você não digitou uma hora valida.')
+
+try:
+    hora_dia_int = int(hora_dia)
+
+    if hora_dia_int >= 0 and hora_dia_int <= 11:
+        print('Bom dia')
+    elif hora_dia_int >= 12 and hora_dia_int <= 17:
+        print('Boa tade')
+    elif hora_dia_int >= 18 and hora_dia_int <= 23:
+        print('Boa noite')
+    else:
+        print('Você não digitou uma hora valida')
+except:
+    print('Por favor digitar apenas números inteiros.')
