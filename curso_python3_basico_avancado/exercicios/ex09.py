71a3f2b774eadd3c6b4217acc15f20f83631099c
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-03-24 16:32:43 -0300

    logica do tratamento de erro e dos operadores para calculadora

--- curso_python3_basico_avancado/exercicios/ex09.py
+++ curso_python3_basico_avancado/exercicios/ex09.py
@@ -10,9 +10,38 @@ logo abaixo uma condição de parada break
 
 
 while True:
+
+    # solicitando os dados para úsuario
+    numero_1 = input('Digite um número: ')
+    numero_2 = input('Digite outro número: ')
+    operador = input('Digite o operador (+-/*): ')
+
+    numeros_validos = None # Bandeira (flag)
+
+    # tratando erros
+    try:
+        numero_1_flot = float(numero_1)
+        numero_2_flot = float(numero_2)
+        numeros_validos = True
+    except:
+        numeros_validos = None
+
+    # condição
+    if numeros_validos is None:
+        print('Um ou ambos os números digitados não são validos.')
+        continue
+
+    operadores_permitidos = '+-/*'
+
+    if operador not in operadores_permitidos:
+        print('Operador invalido.')
+        continue
+
+    if len(operador) > 1:
+        print('Digite apenas 1 operador')
+        continue
+
     sair = input('Quer sair? [s]im: ').lower().startswith('s')
-    # sair = sair.lower()
-    # sair = sair.startwith()
     
     if sair is True:
         break
