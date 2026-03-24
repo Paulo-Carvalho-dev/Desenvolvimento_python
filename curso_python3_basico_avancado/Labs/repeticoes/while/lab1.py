1a76445baeaf43251e4e9304ddea97f9ade20f02
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-03-24 10:22:33 -0300

    refactor: os comentarios para estudo

--- curso_python3_basico_avancado/Labs/repeticoes/while/lab1.py
+++ curso_python3_basico_avancado/Labs/repeticoes/while/lab1.py
@@ -8,9 +8,9 @@ indice = 0
 novo_nome = '' # criando uma variável vazia
 
 # Aqui onde criamos a lógica 
-# enqaunto o indice for menor que tamanho da letra então entre no laço
-# pegando a letra pela posição do indice letra = nome(indice)
-# assim acumulando as interações letra a letra no variável novo_nome
+# enqaunto o indice for menor que tamanho da letra (condição) então entre no laço.
+# pegando a letra pela posição do indice letra, no caso indice iniciado em 0 = nome(indice)
+# assim fazendo a acumulando as interações letra a letra no variável novo_nome
 # por fim imprimindo o valor.
 
 while indice < len(nome): # condição
