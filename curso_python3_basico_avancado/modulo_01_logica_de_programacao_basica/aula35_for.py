42e58c7ea1d4a9d3a937519041bc16ffec990a39
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-04-03 02:18:56 -0300

    aula35 loop for exemplos sobre while

---
+++ curso_python3_basico_avancado/modulo_01_logica_de_programacao_basica/aula35_for.py
@@ -0,0 +1,21 @@
+# texto = 'texto'
+
+# i = 0
+
+# while i < len(texto):
+#     print(texto[i], i)
+
+#     i += 1
+
+
+# senha_salva = '1234'
+# senha_digitada = ''
+# repeticoes = 0
+
+# while senha_salva != senha_digitada:
+#     senha_digitada = input(f'Digite a senha: ({repeticoes})x ')
+
+#     repeticoes += 1
+
+# print('Aquele laço pode ter repetições infinitas. Voce tentou.')
+
