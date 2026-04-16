61c22c0151336baf50c000ba25812bab2194245e
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-04-15 23:44:41 -0300

    (ex11) criado condição para digitar apenas uma letra

--- curso_python3_basico_avancado/exercicios/ex11.py
+++ curso_python3_basico_avancado/exercicios/ex11.py
@@ -15,3 +15,12 @@ Faça a contagem de tentativas do seu
 usuário.
 """
 
+# Criamos uma laço infinito com while
+while True:
+    letra_digitada = input('Digite uma letra: ')
+
+    if len(letra_digitada) > 1:
+        print('Digite apenas uma letra: ')
+        continue
+
+    print('Bora')
