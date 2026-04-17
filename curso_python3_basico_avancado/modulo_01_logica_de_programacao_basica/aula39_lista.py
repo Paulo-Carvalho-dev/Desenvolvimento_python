68e6347040cadbd0ada35ce77476a9fd36466a39
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-04-17 00:30:36 -0300

    (aula39) listas - alterando valor da lista

--- curso_python3_basico_avancado/modulo_01_logica_de_programacao_basica/aula39_lista.py
+++ curso_python3_basico_avancado/modulo_01_logica_de_programacao_basica/aula39_lista.py
@@ -15,4 +15,6 @@ string = 'ABCDE' # 5 caracteres (len)
 #        0    1      2                3    4
 #       -5   -4     -3               -2   -1
 lista = [123, True, 'Paulo Carvalho', 1.2, []]
+lista[-3] = 'Andreia' # alterando valor
+print(lista)
 print(lista[2], type(lista[2]))
