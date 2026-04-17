dfc64e5c572fae9020386cc55c4688242ace0996
Paulo-Carvalho1901 <paulo.carvalho@global.ntt>
2026-04-17 00:28:50 -0300

    (aula39) criando lista e acessando seus elementos pelo indices

--- curso_python3_basico_avancado/modulo_01_logica_de_programacao_basica/aula39_lista.py
+++ curso_python3_basico_avancado/modulo_01_logica_de_programacao_basica/aula39_lista.py
@@ -5,3 +5,14 @@ Suporta vários valores de qualquer tipo
 Conhecimentos reutilizáveis - índices e fatiamento
 Métodos úteis: append, insert, pop, del, clear, extend, +
 """
+#        +01234
+#        -54321
+string = 'ABCDE' # 5 caracteres (len)
+# lista = list()
+# print(bool([])) # falsy
+# print(lista, type(lista))
+
+#        0    1      2                3    4
+#       -5   -4     -3               -2   -1
+lista = [123, True, 'Paulo Carvalho', 1.2, []]
+print(lista[2], type(lista[2]))
